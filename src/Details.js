932cc2d97fec088aa035e496856cf1438d09259d
your_username <MyEmail@Zoho.com>
2021-09-07 09:27:20 +0530

    feat - class component with setstate

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,50 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-    return (
-        <pre>
-            <code>{JSON.stringify(props, null, 4)}</code>
-        </pre>
-    );
+class Details extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true
+        }
+    }
+
+    /* helpful using Ajax req   */
+    // similar to useeffect runs on a first start up 
+    componentDidMount() {
+        pet.animal(this.props.id).then(({ animal }) => {
+            this.setState({
+                name: animal.name,
+                animal: animal.type,
+                location: `${animal.contact.address.city} , ${animal.contact.address.state} `,
+                description: animal.description,
+                media: animal.photos,
+                breed: animal.breeds.primary,
+                loading: false
+            })
+        }, console.error)
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h1>Loading ...</h1>
+        }
+
+        const { name, animal, location, description, breed } = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <button> Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 
